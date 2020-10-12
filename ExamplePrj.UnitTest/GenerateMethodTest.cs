76e850091f507355d36f489852bf05661c0544be
Mojtaba Safavi <sms7182@gmail.com>
2020-10-12 15:43:37 +0330

    Add Project and test for sum and sub methods for sonarexampl

---
+++ ExamplePrj.UnitTest/GenerateMethodTest.cs
@@ -0,0 +1,25 @@
+﻿
+
+using Xunit;
+
+namespace ExamplePrj.UnitTest
+{
+    public class GenerateMethodTest
+    {
+
+        [Fact]
+        public void SumOperatorTest()
+        {
+           
+            var d=GenerateMethod.Sum(1, 3);
+            Assert.Equal(1+3, d);
+        }
+        [Fact]
+        public void SubOperatorTest()
+        {
+
+            var d = GenerateMethod.Sub(1, 3);
+            Assert.Equal(1 -3, d);
+        }
+    }
+}
---
+++ ExamplePrj/GenerateMethod.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace ExamplePrj
+{
+    public static class GenerateMethod
+    {
+        public static int Sum(int a, int b)
+        {
+            return Calculate(a,b,"+");
+        }
+        private static int Calculate(int a,int b,string oprator)
+        {
+            if (oprator == "+")
+            {
+                return a + b;
+            }
+            return a-b;
+        }
+
+        public static int Sub(int a,int b)
+        {
+            return Calculate(a, b, "-");
+        }
+    }
+}
